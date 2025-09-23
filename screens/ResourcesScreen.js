30a3978034584d3d9c319e39d4327d8dc1632788
fralcala <fralcala@iu.edu>
2025-09-23 00:35:44 -0400

    Main project set up

---
+++ screens/ResourcesScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function ResourcesScreen() {
+  return (
+    <View>
+      <Text>ResourcesScreen</Text>
+    </View>
+  );
+}
